78e2b18b859b3331909b7d9e303bb1831b6e05e7
neondoll <bonni_1998@mail.ru>
2024-02-11 20:37:03 +0300

    first commit feedback-form-with-two-fields

---
+++ work-with-html-forms/feedback-form-with-two-fields/index.js
@@ -0,0 +1,13 @@
+const content = document.querySelector('.content');
+
+document.forms[0].addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(event.target.name);
+
+  content.innerHTML = '';
+  content.innerHTML = content.innerHTML + '<p>Имя: ' + event.target.name.value + '</p>';
+  content.innerHTML = content.innerHTML + '<p>Текст: ' + event.target.feedback.value + '</p>';
+
+  event.target.name.value = '';
+  event.target.feedback.value = '';
+});
