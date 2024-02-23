3771450fc3df43e75d4d2e6d963857bf28783d89
neondoll <bonni_1998@mail.ru>
2024-02-23 13:11:56 +0300

    second commit text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,13 @@
 const editorElement = document.getElementById('editor');
 
-editorElement.value = localStorage.getItem('editorValue') || '';
+editorElement.value = localStorage.getItem('editorValue');
 
 const saveEditorValue = (value) => (localStorage.setItem('editorValue', value));
 
 editorElement.addEventListener('input', (event) => (saveEditorValue(event.target.value)));
 
-document.getElementById('clear').addEventListener('click', () => (saveEditorValue(editorElement.value = '')));
+document.getElementById('clear').addEventListener('click', () => {
+  editorElement.value = '';
+
+  saveEditorValue(editorElement.value);
+});
